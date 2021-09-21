18a81a73839456b38999f611c91ac59c7623a4b0
Lorenzo <lorenzopompigna1@gmail.com>
2021-09-21 08:33:32 +0200

    mouse input and coordinates on screen

--- src/main/java/org/um/nine/utils/managers/InputManager.java
+++ src/main/java/org/um/nine/utils/managers/InputManager.java
@@ -1,8 +1,11 @@
 package org.um.nine.utils.managers;
 
 import com.google.inject.Inject;
+import com.jme3.app.SimpleApplication;
 import com.jme3.input.FlyByCamera;
+import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import org.um.nine.contracts.repositories.IBoardRepository;
@@ -20,6 +23,9 @@ public class InputManager {
     private CameraManager cameraManager;
 
     public void init() {
+        gameRepository.getApp().getInputManager().deleteMapping(SimpleApplication.INPUT_MAPPING_MEMORY);
+
+        //camera input mapping
         gameRepository.getApp().getFlyByCamera().unregisterInput();
         gameRepository.getApp().getInputManager().addMapping(Input.LEFT.getName(), Input.LEFT.getTriggers());
         gameRepository.getApp().getInputManager().addMapping(Input.RIGHT.getName(), Input.RIGHT.getTriggers());
@@ -28,7 +34,14 @@ public class InputManager {
         gameRepository.getApp().getInputManager().addMapping(Input.ZOOM_IN.getName(), Input.ZOOM_IN.getTriggers());
         gameRepository.getApp().getInputManager().addMapping(Input.ZOOM_OUT.getName(), Input.ZOOM_OUT.getTriggers());
 
+        //mouse input mapping
+        gameRepository.getApp().getInputManager().addMapping("LClick",new MouseButtonTrigger(0));
+        gameRepository.getApp().getInputManager().addMapping("RClick",new MouseButtonTrigger(1));
+        gameRepository.getApp().getInputManager().addMapping("CClick",new MouseButtonTrigger(2));
+
+
         gameRepository.getApp().getInputManager().addListener(flyByCameraAnalogListener, Input.LEFT.getName(), Input.RIGHT.getName(), Input.UP.getName(), Input.DOWN.getName(), Input.ZOOM_IN.getName(), Input.ZOOM_OUT.getName());
+        gameRepository.getApp().getInputManager().addListener(mouseButtonsListener,"LClick","RClick","CClick");
 
         gameRepository.getApp().getFlyByCamera().setRotationSpeed(2);
         gameRepository.getApp().getFlyByCamera().setMoveSpeed(200);
@@ -56,4 +69,22 @@ public class InputManager {
             }
         }
     };
+
+
+    private final ActionListener mouseButtonsListener = (name, clicking, tpf) -> {
+        switch (name) {
+            case "LClick" -> System.out.println("Left Click");
+            case "RClick" -> System.out.println("Right Click");
+            case "CClick" -> System.out.println("Center Click");
+        }
+        Vector3f positionOfMouseOnClick = gameRepository.getApp().getCamera().getWorldCoordinates(
+                gameRepository.getApp().getInputManager().getCursorPosition(),0);
+
+        System.out.println("Mouse clicked on : "+ positionOfMouseOnClick);
+    };
+
+
+
+
+
 }
