278006397660ba126238ceb7d9cb5c9150c737ec
Kai <kai.krink@gmail.com>
2021-10-02 15:44:22 +0200

    -implemented all 3 steps of the epidemic card

--- src/main/java/org/um/nine/domain/cards/EpidemicCard.java
+++ src/main/java/org/um/nine/domain/cards/EpidemicCard.java
@@ -1,19 +1,66 @@
 package org.um.nine.domain.cards;
+import com.google.inject.Inject;
+import org.um.nine.exceptions.OutbreakException;
+import org.um.nine.repositories.local.BoardRepository;
+import org.um.nine.repositories.local.CardRepository;
+import org.um.nine.repositories.local.DiseaseRepository;
+import org.um.nine.utils.cardmanaging.Shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 public class EpidemicCard extends PlayerCard {
+
+    @Inject
+    private BoardRepository boardRepository;
+
+    @Inject
+    private CardRepository cardRepository;
+
+    @Inject
+    private DiseaseRepository diseaseRepository;
+
     public EpidemicCard(String name) {
         super(name);
     }
 
     public void increase() {
-        // TODO: Move the infection rate indicator up by 1
+        boardRepository.getInfectionRateMarker().setCount(boardRepository.getInfectionRateMarker().getCount()+1);
     }
 
     public void infect() {
-        // TODO: Draw a card off  the bottom of the infection draw pile and infect the indicated city with 3 cubes. Discard the card.
+        for(int i = 0; i < 3; i++){
+            InfectionCard infectionCard = cardRepository.getInfectionDeck().get(cardRepository.getInfectionDeck().size()-1);
+            cardRepository.getInfectionDeck().remove(cardRepository.getInfectionDeck().size()-1);
+            try{
+                int amountCubes = infectionCard.getCity().getCubes().size();
+                switch (amountCubes){
+                    case 2:
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        break;
+                    case 3:
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        break;
+                    default:
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        diseaseRepository.infect(infectionCard.getCity().getColor(), infectionCard.getCity());
+                        break;
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public void intensify() {
-        // TODO: Shuffle the infection discard pile and place it on top of the infection draw pile.
+        Stack<InfectionCard> pile = cardRepository.getInfectionDiscardPile();
+        Collections.shuffle(pile);
+        cardRepository.getInfectionDeck().addAll(pile);
+        cardRepository.setInfectionDiscardPile(new Stack<>());
     }
 }
