5b6a18aa99b0d54c7a239fafee5ce76d2a638727
Noah <nscharrenberg@hotmail.com>
2021-10-26 10:00:52 +0200

    agent stuff

---
+++ src/main/java/org/um/nine/agents/baseline/BaselineAgent.java
@@ -0,0 +1,5 @@
+package org.um.nine.agents.baseline;
+
+public class BaselineAgent {
+    // TODO: Add Baseline Agent Logic here
+}
---
+++ src/main/java/org/um/nine/agents/reinforcement/ReinforcementAgent.java
@@ -0,0 +1,5 @@
+package org.um.nine.agents.reinforcement;
+
+public class ReinforcementAgent {
+    // TODO: Add Reinforcement learning Agent logic here
+}
