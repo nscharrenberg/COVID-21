2ea70639c2a9342f036605c55edbd3fdc3070816
Kai <kai.krink@gmail.com>
2021-10-27 20:20:59 +0200

    implemented "PrognosisEvent" card!

--- src/main/java/org/um/nine/config/GuiceConfiguration.java
+++ src/main/java/org/um/nine/config/GuiceConfiguration.java
@@ -38,6 +38,7 @@ public class GuiceConfiguration extends AbstractModule {
     private final DiscardCardDialog discardCardDialog = new DiscardCardDialog();
     private final ContingencyPlannerState contingencyPlannerState = new ContingencyPlannerState();
     private final GameEndState gameEndState = new GameEndState();
+    private final PrognosisEventDialog prognosisEventDialog = new PrognosisEventDialog();
 
     @Override
     protected void configure() {
@@ -66,5 +67,6 @@ public class GuiceConfiguration extends AbstractModule {
         bind(DiscardCardDialog.class).toInstance(discardCardDialog);
         bind(ContingencyPlannerState.class).toInstance(contingencyPlannerState);
         bind(GameEndState.class).toInstance(gameEndState);
+        bind(PrognosisEventDialog.class).toInstance(prognosisEventDialog);
     }
 }
--- src/main/java/org/um/nine/domain/cards/events/PrognosisEvent.java
+++ src/main/java/org/um/nine/domain/cards/events/PrognosisEvent.java
@@ -1,14 +1,31 @@
 package org.um.nine.domain.cards.events;
 
+import com.google.inject.Inject;
+import org.um.nine.contracts.repositories.ICardRepository;
 import org.um.nine.domain.cards.EventCard;
+import org.um.nine.domain.cards.InfectionCard;
+import org.um.nine.exceptions.GameOverException;
+
+import java.util.ArrayList;
 
 public class PrognosisEvent extends EventCard {
     public PrognosisEvent() {
-        super("Prognosis event", "action");
+        super("Prognosis event", "Draw, look at, rearrange the top 6 cards of the infection deck. Put the m back on top");
     }
+
+    @Inject
+    private ICardRepository cardRepository;
 
     @Override
     public void event() {
-
+        ArrayList<InfectionCard> topCards = new ArrayList<>();
+        for(int i = 0; i < 6; i++){
+            try{
+                topCards.add(cardRepository.getInfectionDeck().pop());
+            } catch (Exception e){
+                topCards.forEach(c -> cardRepository.getInfectionDeck().add(c));
+                e.printStackTrace();
+            }
+        }
     }
 }
