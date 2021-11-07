47676c064df93f4a341735b796d7cdc619b747d6
Noah Scharrenberg <nscharrenberg@hotmail.com>
2021-11-07 19:29:54 +0100

    remove debug lines

--- src/main/java/org/um/nine/domain/Disease.java
+++ src/main/java/org/um/nine/domain/Disease.java
@@ -10,12 +10,8 @@ public class Disease {
 
     public Disease(ColorRGBA color) {
         cubeNumber++;
-        if(cubeNumber>=96) {
-            System.out.println();
-        }
         this.color = color;
         number = cubeNumber;
-        System.out.println("cubeNumber: "+cubeNumber);
     }
 
     public ColorRGBA getColor() {
@@ -43,7 +39,7 @@ public class Disease {
         sb.append(this.getColor().toString());
         sb.append("-");
         sb.append(number);
-        System.out.println(cubeNumber + " " + number + " from tostring");
+        
         return sb.toString();
     }
 }
