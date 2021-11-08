bb0fc433883f48b390b30851acba644b8e832b27
Lorenzo <lorenzopompigna1@gmail.com>
2021-11-08 13:34:15 +0100

    agent configuration

--- src/main/java/org/um/nine/agents/baseline/BaselineAgent.java
+++ src/main/java/org/um/nine/agents/baseline/BaselineAgent.java
@@ -1,13 +1,17 @@
 package org.um.nine.agents.baseline;
 
 import com.google.inject.Inject;
+import org.um.nine.contracts.repositories.IBoardRepository;
+import org.um.nine.contracts.repositories.ICityRepository;
 import org.um.nine.contracts.repositories.IPlayerRepository;
+import org.um.nine.domain.ActionType;
 import org.um.nine.domain.City;
 import org.um.nine.domain.Player;
 import org.um.nine.domain.cards.CityCard;
 import org.um.nine.domain.cards.PlayerCard;
 import org.um.nine.domain.roles.MedicRole;
-import org.um.nine.exceptions.InvalidMoveException;
+import org.um.nine.domain.roles.RoleAction;
+import org.um.nine.exceptions.*;
 
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -15,9 +19,16 @@ import java.util.stream.Collectors;
 
 
 public class BaselineAgent {
-    
+
     @Inject
     private IPlayerRepository playerRepository;
+    @Inject
+    private IBoardRepository boardRepository;
+
+    @Inject
+    private ICityRepository cityRepository;
+
+
 
     public boolean moveToCity(Player currentPlayer, City target) throws InvalidMoveException {
         if (currentPlayer.getCity().getName().equals(target.getName())) return true;
@@ -55,50 +66,86 @@ public class BaselineAgent {
 
 
     public void randomAction(Player player) {
-        int possibleActions = 6;
+        int possibleActions = 9;
         int random = new Random().nextInt(possibleActions);
 
-        try {
+        ActionType selectedAction = null;
+        RoleAction roleAction = null;
+        System.out.println(random);
 
             switch (random) {
                 case 0 -> {
                     City next = player.getCity().getNeighbors().get(0);
-                    playerRepository.drive(player, next);
-                    System.out.println("Agent moved to "+ next.getName());
+                    selectedAction = ActionType.DRIVE;
+                    boardRepository.setSelectedCity(next);
+                    System.out.println("Agent moving to "+ next.getName());
                 }
                 case 1 -> {
                     PlayerCard toMove = player.getHandCards().stream().filter(c -> c instanceof CityCard).findFirst().orElse(null);
                     if (toMove != null) {
                         City next = ((CityCard) toMove).getCity();
-                        playerRepository.shuttle(player, next);
-                        System.out.println("Agent shuttled to "+ next.getName());
+                        selectedAction = ActionType.CHARTER_FLIGHT;
+                        boardRepository.setSelectedCity(next);
                     }
                 }
-                case 2 -> {
-                    if (!player.getCity().getCubes().isEmpty()) {
-                        playerRepository.treat(player, player.getCity());
-                        System.out.println("Agent cured 1 disease cube");
+                case  2 -> {
+                    CityCard cityCard = (CityCard) player.getHandCards().stream().filter(c -> c instanceof CityCard).findFirst().orElse(null);
+                    if (cityCard!= null){
+                        boardRepository.setSelectedCity(cityCard.getCity());
+                        selectedAction = ActionType.DIRECT_FLIGHT;
                     }
                 }
                 case 3 -> {
-                    //TODO : USE ROLE ACTION IMPLEMENTATION
+                    if (cityRepository.getCities().values().stream().filter(c -> c.getResearchStation()!= null).count() >=2 &&
+                            player.getCity().getResearchStation() != null){
+                        City city = cityRepository.getCities().values().stream().filter( c-> c.getResearchStation() != null && !c.equals(player.getCity())).findFirst().orElse(null);
+                        if (city!= null){
+                            boardRepository.setSelectedCity(city);
+                            selectedAction = ActionType.SHUTTLE;
+                        }
+                    }
                 }
                 case 4 -> {
-                    var sameColorCard = player.getHandCards().stream().filter(c -> c instanceof CityCard).collect(Collectors.groupingBy(c -> ((CityCard)c).getCity().getColor()));
-                    if (sameColorCard.values().size()>=5 || (player.getRole() instanceof MedicRole) && sameColorCard.values().size() >=4) {
-                        //TODO: add discover cure of the color
-                        sameColorCard.entrySet().forEach(System.out::println);
+                    if (player.getCity().getResearchStation() == null && player.getHandCards().stream().filter(c -> {
+                        if (c instanceof CityCard card) return card.getCity().equals(player.getCity());
+                        return false;
+                    }).findFirst().orElse(null) != null) {
+                        selectedAction = ActionType.BUILD_RESEARCH_STATION;
                     }
                 }
                 case 5 -> {
-                    //playerRepository.share(player,player.getCity());
+                    if (!player.getCity().getCubes().isEmpty()) {
+                        boardRepository.setSelectedCity(player.getCity());
+                        selectedAction = ActionType.TREAT_DISEASE;
+                    }
                 }
-                default -> { }
+                case 6 -> {
+                    //TODO : implement share knowledge automatic approval
+                }
+                case 7 -> {
+                    var sameColorCard = player.getHandCards().stream().filter(c -> c instanceof CityCard).collect(Collectors.groupingBy(c -> ((CityCard)c).getCity().getColor()));
+                    if (sameColorCard.values().size()>=5 || (player.getRole() instanceof MedicRole) && sameColorCard.values().size() >=4) {
+                        sameColorCard.entrySet().forEach(System.out::println);
+                        selectedAction = ActionType.DISCOVER_CURE;
+                    }
+                }
+                case 8 -> {
+                    for (RoleAction r : RoleAction.values()){
+                        if (player.getRole().actions(r))
+                            roleAction = r;
+                    }
+                }
+                default -> {
+                    selectedAction = ActionType.SKIP_ACTION;
+                }
             }
-        } catch (InvalidMoveException e) {
+        boardRepository.setSelectedPlayerAction(selectedAction == null? ActionType.SKIP_ACTION : selectedAction);
+        boardRepository.setSelectedRoleAction(roleAction == null ? RoleAction.NO_ACTION : roleAction);
+        try {
+            playerRepository.action(boardRepository.getSelectedPlayerAction());
+        } catch (InvalidMoveException | NoActionSelectedException | ResearchStationLimitException | CityAlreadyHasResearchStationException | NoCubesLeftException | NoDiseaseOrOutbreakPossibleDueToEvent | GameOverException e) {
             e.printStackTrace();
         }
-
 
     }
 }
