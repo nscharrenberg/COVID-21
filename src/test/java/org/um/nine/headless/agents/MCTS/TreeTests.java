e3d6fbf493c99c9550cb2a37899fd5d6537d2d65
Kai <kai.krink@gmail.com>
2022-01-10 18:54:25 +0100

    added execute

--- src/test/java/org/um/nine/headless/agents/MCTS/TreeTests.java
+++ src/test/java/org/um/nine/headless/agents/MCTS/TreeTests.java
@@ -4,25 +4,47 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.um.nine.headless.agents.mcts.Actions;
 import org.um.nine.headless.agents.mcts.Node;
+import org.um.nine.headless.agents.state.IState;
 import org.um.nine.headless.agents.state.State;
+import org.um.nine.headless.agents.utils.ExperimentalGame;
+import org.um.nine.headless.game.exceptions.PlayerLimitException;
 
 public class TreeTests {
     @Test
     public void rootTest() {
-        Node root = new Node(new State());
+        ExperimentalGame game = new ExperimentalGame();
+        try{
+            game.getCurrentState().getPlayerRepository().createPlayer("P1",true);
+            game.getCurrentState().getPlayerRepository().createPlayer("P2",true);
+        } catch (PlayerLimitException e) {
+            e.printStackTrace();
+        }
+        game.start();
+        IState state = game.getCurrentState();
+
+        Node root = new Node(state);
         Assertions.assertEquals(root.isRoot(),true);
-        Node child = new Node(new State(), root, Actions.DRIVE);
+        Node child = new Node(state, root, Actions.DRIVE);
         Assertions.assertEquals(child.isRoot(),false);
         Assertions.assertEquals(child.getParent().isRoot(),true);
     }
 
     @Test
     public void childTests(){
-        Node root = new Node(new State());
+        ExperimentalGame game = new ExperimentalGame();
+        try{
+            game.getCurrentState().getPlayerRepository().createPlayer("P1",true);
+            game.getCurrentState().getPlayerRepository().createPlayer("P2",true);
+        } catch (PlayerLimitException e) {
+            e.printStackTrace();
+        }
+        game.start();
+        IState state = game.getCurrentState();
+        Node root = new Node(state);
         Assertions.assertEquals(root.getChildren().isEmpty(),true);
-        Node child1 = new Node(new State(), root, Actions.DRIVE);
-        Node child2 = new Node(new State(), root, Actions.DRIVE);
-        Node child3 = new Node(new State(), root, Actions.DRIVE);
+        Node child1 = new Node(state, root, Actions.DRIVE);
+        Node child2 = new Node(state, root, Actions.DRIVE);
+        Node child3 = new Node(state, root, Actions.DRIVE);
         Assertions.assertEquals(root.getChildren().contains(child1),true);
         Assertions.assertEquals(root.getChildren().contains(child2),true);
         Assertions.assertEquals(root.getChildren().contains(child3),true);
@@ -30,9 +52,18 @@ public class TreeTests {
 
     @Test
     public void leafTest(){
-        Node root = new Node(new State());
+        ExperimentalGame game = new ExperimentalGame();
+        try{
+            game.getCurrentState().getPlayerRepository().createPlayer("P1",true);
+            game.getCurrentState().getPlayerRepository().createPlayer("P2",true);
+        } catch (PlayerLimitException e) {
+            e.printStackTrace();
+        }
+        game.start();
+        IState state = game.getCurrentState();
+        Node root = new Node(state);
         Assertions.assertEquals(root.isLeaf(),true);
-        Node child = new Node(new State(), root, Actions.DRIVE);
+        Node child = new Node(state, root, Actions.DRIVE);
         Assertions.assertEquals(root.isLeaf(),false);
         Assertions.assertEquals(child.isLeaf(),true);
     }
