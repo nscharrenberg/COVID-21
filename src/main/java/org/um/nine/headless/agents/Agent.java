a42d27e7cd55c56b1a6ae74923d899c942231655
Kai <kai.krink@gmail.com>
2022-01-12 00:19:51 +0100

    commit

--- src/main/java/org/um/nine/headless/agents/Agent.java
+++ src/main/java/org/um/nine/headless/agents/Agent.java
@@ -1,7 +1,10 @@
 package org.um.nine.headless.agents;
 
 import org.um.nine.headless.agents.state.IState;
+import org.um.nine.headless.agents.utils.Log;
 
 public interface Agent{
     void agentDecision(IState state) throws Exception;
+
+    Log getLog();
 }
